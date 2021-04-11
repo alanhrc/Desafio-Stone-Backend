9a22f2f6a2bc4ccb9391b3414d7470083180ba18
Alan Henrique <alancamargo50@gmail.com>
2021-04-11 14:13:10 -0300

    finish desafio

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,21 @@
 module.exports = [
   {
-    "environment": "production",
-    "name": "default",
-    "type": "postgres",
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "entities": [
+    environment: "production",
+    name: "default",
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [
       "./src/modules/**/infra/typeorm/entities/*.ts"
     ],
-    "migrations": [
-      "./src/shared/infra/typeorm/migrations/*.ts"
+    migrations: [
+      "/src/shared/infra/typeorm/migrations/*.ts"
     ],
-    "cli": {
-      "migrationsDir": "./src/shared/infra/typeorm/migrations"
+    cli: {
+      migrationsDir: "./src/shared/infra/typeorm/migrations"
     }
   },
   {
