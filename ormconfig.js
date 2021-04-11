345fa7de510cd4fa75a1b799b3b3b66f43705b64
Alan Henrique <alancamargo50@gmail.com>
2021-04-11 14:33:44 -0300

    finish desafio

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    environment: "production",
+    environment: "development",
     name: "default",
     type: "postgres",
     host: process.env.DB_HOST,
@@ -19,22 +19,22 @@ module.exports = [
     }
   },
   {
-    "environment": "development",
-    "name": "default",
-    "type": "postgres",
-    "host": "localhost",
-    "port": "5432",
-    "username": "stone",
-    "password": "stone",
-    "database": "stonedb",
-    "entities": [
+    environment: "production",
+    name: "default",
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [
       "./dist/modules/**/infra/typeorm/entities/*.js"
     ],
-    "migrations": [
-      "./dist/shared/infra/typeorm/migrations/*.js"
+    migrations: [
+      "/dist/shared/infra/typeorm/migrations/*.js"
     ],
-    "cli": {
-      "migrationsDir": "./dist/shared/infra/typeorm/migrations"
+    cli: {
+      migrationsDir: "./dist/shared/infra/typeorm/migrations"
     }
   }
 ]
